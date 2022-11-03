fff61ccc2bb6977081556a18a89b91594ba7bb19
Dirk Moeller <moeller.dirk80@gmail.com>
2022-11-03 20:50:44 +0100

    add uartmasterslave to imports

--- rtl/z80computer.v
+++ rtl/z80computer.v
@@ -10,38 +10,130 @@ module z80computer(
     output o_cs,
     input i_ack,
     input i_int,
-    input i_nmi
+    input i_nmi,
+    input i_uart_rx,
+    output o_uart_tx
 );
 
-wire w_m1_n;
-wire w_mreq_n;
-wire w_iorq_n;
-wire w_rd_n;
-wire w_wr_n;
+// wire vgamaster_access;
+// reg r_vgamaster_active;
+reg r_uartmaster_active;
+reg r_cpumaster_active;
+
+// CPU
+wire cpu_m1;
+wire cpu_mreq_n;
+wire cpu_iorq_n;
+wire cpu_rd_n;
+wire cpu_wr_n;
+wire cpu_we = ~cpu_wr_n & (~cpu_mreq_n | ~cpu_iorq_n);
+wire cpu_cs = (~cpu_wr_n | ~cpu_rd_n) & (~cpu_mreq_n | ~cpu_iorq_n | ~cpu_m1);
+wire reset;
+wire cpu_wait;
+wire cpu_ack;
+wire [15:0] o_cpu_addr;
+wire [7:0] o_cpu_dat;
+wire cpu_int;
 
 tv80s #(.Mode(1), .T2Write(1), .IOWait(0)) cpu0 (
   .clk(i_clk),
-  .reset_n(~i_reset),
+  .reset_n(~reset),
 
-  .m1_n(w_m1_n),
-  .mreq_n(w_mreq_n),
-  .iorq_n(w_iorq_n),
-  .rd_n(w_rd_n),
-  .wr_n(w_wr_n),
+  .m1_n(cpu_m1),
+  .mreq_n(cpu_mreq_n),
+  .iorq_n(cpu_iorq_n),
+  .rd_n(cpu_rd_n),
+  .wr_n(cpu_wr_n),
   .rfsh_n(),
   .halt_n(),
   .busak_n(),
-  .A(o_addr),
-  .do(o_dat),
+  .A(o_cpu_addr),
+  .do(o_cpu_dat),
   .di(i_dat),
-  .wait_n(o_cs ? i_ack : 1'b1),
-  .int_n(~i_int),
+  .wait_n(~cpu_wait),
+  .int_n(~cpu_int),
   .nmi_n(~i_nmi),
   .busrq_n(1'b1)
 );
 
-assign o_we = ~w_wr_n & (~w_mreq_n | ~w_iorq_n);
-assign o_cs = (~w_wr_n | ~w_rd_n) & (~w_mreq_n | ~w_iorq_n | ~w_m1_n);
+// UART master/slave
+wire [15:0] o_uartmaster_addr;
+wire [7:0] o_uartmaster_dat;
+wire uartmaster_we;
+wire uartmaster_cs;
+wire uartmaster_ack;
 
+wire [7:0] o_uartslave_dat;
+wire o_uartslave_ack;
+wire i_uartslave_we;
+wire i_uartslave_cs;
+
+wire o_uart_reset;
+wire o_uart_int;
+
+UartMasterSlave #(.BAUDRATE(115200),.SYS_FREQ(25000000)) uart(
+    .i_clk(i_clk),
+    .i_reset(i_reset),
+
+    .i_master_data(i_dat),
+    .o_master_data(o_uartmaster_dat),
+    .o_master_addr(o_uartmaster_addr),
+    .i_master_ack(uartmaster_ack),
+    .o_master_we(uartmaster_we),
+    .o_master_cs(uartmaster_cs),
+
+    .i_slave_data(i_dat),
+    .o_slave_data(o_uartslave_dat),
+    .i_slave_addr(o_addr[0]),
+    .o_slave_ack(o_uartslave_ack),
+    .i_slave_we(i_uartslave_we),
+    .i_slave_cs(i_uartslave_cs),
+    .o_int(o_uart_int),
+
+    .i_uart_rx(i_uart_rx),
+    .o_uart_tx(o_uart_tx),
+
+    .o_reset(o_uart_reset)
+);
+
+// multi-master handling
+
+assign reset = o_uart_reset || i_reset;
+assign cpu_wait = ~cpu_ack;
+
+// note: vgamaster never outputs data
+assign          o_dat = r_uartmaster_active ? o_uartmaster_dat :
+                         r_cpumaster_active ? o_cpu_dat : 0;
+
+assign         o_addr =  //r_vgamaster_active ? o_vgamaster_addr :
+                        r_uartmaster_active ? o_uartmaster_addr :
+                         r_cpumaster_active ? o_cpu_addr : 0;
+
+assign           o_we =   //r_vgamaster_active ? 1'b0 :
+                         r_uartmaster_active ? uartmaster_we :
+                          r_cpumaster_active ? cpu_we : 0;
+
+assign           o_cs =   //r_vgamaster_active ? vgamaster_cs :
+                         r_uartmaster_active ? uartmaster_cs :
+                          r_cpumaster_active ? 1'b1 : 0;
+
+assign cpu_ack      = r_cpumaster_active && cpu_cs && i_ack;
+assign uartmaster_ack = ~r_uartmaster_active && uartmaster_cs && i_ack;
+
+always @(posedge i_clk)
+begin
+    // r_vgamaster_active <= 0;
+    r_uartmaster_active <= 0;
+    r_cpumaster_active <= 0;
+    // if (vgamaster_access)
+    //     r_vgamaster_active  <= 1; // vga has highest bus priority
+    //else
+    if (uartmaster_cs)
+        r_uartmaster_active <= 1;
+    else
+        r_cpumaster_active  <= 1; // cpu has lowest priority
+end
+
+assign cpu_int = i_int || o_uart_int;
 
 endmodule
